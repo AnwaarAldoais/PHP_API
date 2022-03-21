5b36bde4fb599f489d1fbedddd1e6633160264e9
AnwaarAldoais <82754054+AnwaarAldoais@users.noreply.github.com>
2022-03-21 11:33:28 +0300

    Update syncDailyOperationsToServer.php

--- syncDailyOperationsToServer.php
+++ syncDailyOperationsToServer.php
@@ -34,7 +34,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     $rowsTotal++;
                 }
 
-
                 $sql = "SELECT column_name FROM information_schema.columns WHERE table_name = '$obj->table'";
                 $stmt = $conn->prepare($sql);
 
@@ -55,13 +54,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                 }
                             }
                         }
-
-
                         // start sync data with client by Insertion
                         $sql = "INSERT INTO $obj->table ($cx)";
                         $sql .= "VALUES";
                         $sql .= $ccV;
-
+                        // echo $sql;
                         $stmt = $conn->prepare($sql);
                         if ($stmt->execute()) {
                             $id = $conn->lastInsertId();
@@ -72,7 +69,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                             if ($stmt->execute()) {
                                 //fetch effcted rows 
                                 $number_of_rows = $stmt->fetchColumn();
-
+                                $number_of_rows += $number_of_rows;
                                 header("rowsTotal:$rowsTotal");
                                 header("rowsEffected:$number_of_rows");
 
@@ -112,56 +109,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     $sql = "SELECT * FROM $obj->table WHERE id=$values->id AND sync_status='editServer' ";
                     $stmt = $conn->prepare($sql);
 
-                    if ($stmt->execute()) {
-                        try {
+                    try {
+                        if ($stmt->execute()) {
                             $raw_column_data = $stmt->fetch(PDO::FETCH_ASSOC);
-
                             foreach ($values as $columns => $columnsValue) {
-                                // if (strtotime($values->updated_at) > $raw_column_data["updated_at"]) {
+                                if (strtotime($values->updated_at) > $raw_column_data["updated_at"]) {
+                                    $sql = "UPDATE $obj->table set $columns='$columnsValue' WHERE id=$values->id";
+                                    $stmt = $conn->prepare($sql);
+                                    if ($stmt->execute()) {
+                                        header("rowsTotal:$rowsTotal");
+                                        header("rowsEffected:$number_of_rows");
 
-                                // $co[] = $columns;
-                                // $va[$columns] = $columnsValue;
-
-                                // $cc = implode(",", $co);
-                                // $cx = trim($cc, '"');
-                                // //some operations to shift syntax of values into => (a,b,c,d,e) (a,b,c,d,e)
-                                // $ccV = implode(",", $va);
-                                // $ccV = trim($ccV, '"');
-                                $sql = "UPDATE $obj->table set $columns='$columnsValue' WHERE id=$values->id";
-                                $stmt = $conn->prepare($sql);
-                                if ($stmt->execute()) {
-                                    echo "yes";
+                                        echo "Sync data with server successfully";
+                                    }
                                 }
-                                // $stmt = $conn->prepare($sql);
-
-                                // }   
                             }
-                        } catch (Exception $e) {
-                            return $e->getMessage();
                         }
+                    } catch (Exception $e) {
+                        return $e->getMessage();
                     }
                 }
-                // echo $cx;
-                // echo  json_encode($values)."</br>";   
-                // $sql = "UPDATE $obj->table set $cx=?";
-                // $sql .=$ccV;
-                // $sql .=" WHERE id=$values->id";
-                // $stmt = $conn->prepare($sql);
-
-                // $stmt->bindParam("ssssi", $ccV);
-                // echo $sql . "</br>";
-                // //  echo $sql;
-                // if ($stmt->execute()) {
-                //     //fetch effcted rows 
-                //     // $number_of_rows = $stmt->rowCount();
-
-                //     // header("rowsTotal:$rowsTotal");
-                //     // header("rowsEffected:$number_of_rows");
-
-                //     echo "Sync data with server successfully";
-                // }
-                // echo  $cx . "</br>";
-                // echo  json_encode($va) . "</br>";
             }
         }
     }
