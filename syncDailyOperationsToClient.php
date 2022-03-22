3a1e1229a2d28b54492da1a043c50de3f6b73044
AnwaarAldoais <82754054+AnwaarAldoais@users.noreply.github.com>
2022-03-22 08:47:30 +0300

    Update syncDailyOperationsToClient.php

--- syncDailyOperationsToClient.php
+++ syncDailyOperationsToClient.php
@@ -8,15 +8,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 
-// $status1='newServerOnly';
-// $status2='editServerOnly';
-// $status3='delServerOnly';
-// $pr=[
-//     ":status1"=>$status1,
-//     ":status2"=>$status2,
-//     ":status3"=>$status3,
-// ];
-$sync_status = array('newServerOnly', 'editServerOnly', 'delServerOnly');
+$sync_status = array('newServer', 'editServer', 'deleServer');
 $param = str_repeat("?,", count($sync_status) - 1) . "?";
 
 // start sync daily operations between server and client =:status1 OR sync_status=:status2 OR sync_status=:status3
@@ -29,26 +21,21 @@ $stmt = $conn->prepare($sql);
 try {
     if ($stmt->execute()) {
 
-        foreach ($row = $stmt->fetchAll(PDO::FETCH_ASSOC) as $outer_key => $array) {
+        foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $outer_key => $array) {
             foreach ($array as $inner_key => $value) {
 
-
-                $sql = "Select *  FROM `$value` c WHERE  `sync_status` IN ($param)";
+                $sql = "Select *  FROM `$value`  WHERE  `sync_status` IN ($param)";
                 $stmt = $conn->prepare($sql);
-
                 if ($stmt->execute($sync_status)) {
                     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                    foreach ($result as $row => $rowValue) {
-                        $data[$value] = $rowValue;
-                        echo json_encode($data);
-                    }
-                }
+                    $data[$value] = $result;   
+                }                   
             }
-        }
+        }             
     }
 } catch (Exception $e) {
     return $e->getMessage();
 }
-
+echo json_encode($data);
 ?>
 <!-- function  -->
